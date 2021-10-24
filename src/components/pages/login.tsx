9725b6ac7caaf969786d377177715946142e9b01
mamecom <mamesan.jp@gmail.com>
2021-10-25 00:34:07 +0900

    【プロトタイプ】認証画面 新規作成、ログイン画面の画面表示、繊維のみ（ボタンコンポーネントなし）

---
+++ src/components/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GenericTemplate from "../templates/GenericTemplate";
+
+const Login: React.FC = () => {
+  return (
+    <GenericTemplate title="ログイン">
+      <>●ログインID、パスワードを入力してください</>
+    </GenericTemplate>
+  );
+};
+
+export default Login;
